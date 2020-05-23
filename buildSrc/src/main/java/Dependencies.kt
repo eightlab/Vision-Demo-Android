37daedfabd570f674101c623eacfdf9b7f236826
munnadroid <gotameraju2@gmail.com>
2020-05-23 20:12:18 +0545

    Format dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -2,7 +2,7 @@ object Versions {
     const val kotlinVersion = "1.3.41"
     const val appCompat = "1.1.0"
     const val gson = "2.8.6"
-    const val eightVision="1.0.1'"
+    const val eightVision = "1.0.1"
 }
 
 object Dependencies {
@@ -11,5 +11,5 @@ object Dependencies {
     const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.appCompat}"
     const val gson = "com.google.code.gson:gson:${Versions.gson}"
 
-    const val eightVision="'co.eightlab:eightvision:${Versions.eightVision}"
+    const val eightVision = "co.eightlab:eightvision:${Versions.eightVision}"
 }
